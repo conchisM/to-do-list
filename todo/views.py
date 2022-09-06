85a0955d347669167c1341155324f3038cc0a9af
Kostya Antonov <kostya_antonov_04@mail.ru>
2022-09-06 17:45:17 +0500

    POST and GET processing

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
+
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html')
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'That username has already been taken'})
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Password did not mach'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.htmlcurr')
