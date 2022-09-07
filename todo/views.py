35824e0950e3d04560dc19df1edcb87229ad113f
Kostya Antonov <kostya_antonov_04@mail.ru>
2022-09-07 16:41:23 +0500

    added createtodo and fixed some bags

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
 
 
 def home(request):
@@ -19,17 +20,28 @@ def signupuser(request):
                 login(request, user)
                 return redirect('currenttodos')
             except IntegrityError:
-                return render(request, 'todo/signupuser.html',
-                              {'form': UserCreationForm(), 'error': 'That username has already been taken'})
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'That username has already been taken'})
         else:
-            return render(request, 'todo/signupuser.html',
-                          {'form': UserCreationForm(), 'error': 'Password did not mach'})
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Password did not mach'})
 
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username or password is not march'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+def createtodo(request):
+    return render(request, 'todo/createtodo.html', {'form': TodoForm()})
 
 
 def currenttodos(request):
