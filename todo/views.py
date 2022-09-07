48c7085a7ea24cd604d60ff3f498b573f56c58b4
Kostya Antonov <kostya_antonov_04@mail.ru>
2022-09-07 17:35:13 +0500

    edit currenttodos.html

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
 
 
 def home(request):
@@ -41,8 +42,18 @@ def logoutuser(request):
         return redirect('home')
 
 def createtodo(request):
-    return render(request, 'todo/createtodo.html', {'form': TodoForm()})
-
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Bad data'})
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted_isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
